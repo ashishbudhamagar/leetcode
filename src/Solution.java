4172c94880c3621b20289fd32faf9d9199be6281
AshGitHubAcc <budha.a@northeastern.edu>
2026-04-29 14:21:35 -0400

    info for previous

--- src/Solution.java
+++ src/Solution.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class Solution {
 
     public boolean containsDuplicate(int[] nums) {
+        // Time: O(n) because theres a for loop
+        // Space: O(n) because worst space is all the values
+        //   in the arrays get put inside the map
 
         Set<Integer> set = new HashSet<>();
 
